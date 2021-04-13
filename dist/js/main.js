a28ce611ed8aeee56d919fc96e23d155c32a4455
MohamedElrodainy <mohamedelrodainy1@gmail.com>
2021-04-13 03:10:04 +0200

    Lesson 13/14

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,4 +5,11 @@ $(function() {
     $('.toggle-sidebar').on('click', function() {
         $('.content-area, .sidebar').toggleClass('no-sidebar');
     });
+
+    $('.toggle-submenu').on('click', function() {
+
+        $(this).find('.fa-angle-right').toggleClass('down');
+
+        $(this).next('.child-links').slideToggle();
+    });
 });
